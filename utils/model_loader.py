74fdf1d3e6b02a797e56ce947ce9bb72ef3723f9
Jobin luckose <jobinluckose01@gmail.com>
2025-06-21 00:32:40 +0530

    Fix: Force CPU mode for Streamlit deployment

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -6,4 +6,4 @@ from transformers import pipeline
 
 
 def load_summarizer():
-    return pipeline("summarization",model ="facebook/bart-large-cnn",framework="pt",device=-1)
+    return pipeline("summarization",model ="facebook/bart-large-cnn",framework="pt",device=-1,model_kwargs={"torch_dtype": "float32"})
