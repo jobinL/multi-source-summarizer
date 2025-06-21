98576a5bbf422cd5cfb3ff96192f72391a00d7c6
Jobin luckose <jobinluckose01@gmail.com>
2025-06-21 22:52:06 +0530

    Fix:PDF reading error fixed(I guess:) )

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -9,5 +9,4 @@ def load_summarizer():
         model="facebook/bart-large-cnn",
         framework="pt",           # Use PyTorch
         device=-1,                # Force CPU
-        model_kwargs={"torch_dtype": "float32"}  # Safe precision for CPU
     )
