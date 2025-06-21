312919e300fbb804d3efc67ad5376b51aadaaf9b
Jobin luckose <jobinluckose01@gmail.com>
2025-06-21 22:43:27 +0530

    Fix:PDF reading error fixed(I guess:) )

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -3,7 +3,11 @@ os.environ["TRANSFORMERS_NO_TF"] = "1"
 
 from transformers import pipeline 
 
-
-
 def load_summarizer():
-    return pipeline("summarization",model ="facebook/bart-large-cnn",framework="pt",device=-1,model_kwargs={"torch_dtype": "float32"})
+    return pipeline(
+        "summarization",
+        model="facebook/bart-large-cnn",
+        framework="pt",           # Use PyTorch
+        device=-1,                # Force CPU
+        model_kwargs={"torch_dtype": "float32"}  # Safe precision for CPU
+    )
