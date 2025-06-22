3d93dd1b0a77bebe6158ad88217ba4ffd3e58b43
Jobin luckose <jobinluckose01@gmail.com>
2025-06-22 22:04:18 +0530

    Fix 1:Deployement issue

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -1,13 +1,19 @@
 import os
 os.environ["TRANSFORMERS_NO_TF"] = "1"
 
-from transformers import pipeline 
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer,pipeline
 
 def load_summarizer():
+    model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn")
+    tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
+    
+    # Explicitly move model to CPU
+    model = model.to("cpu")
+    
     return pipeline(
         "summarization",
-        model="facebook/bart-large-cnn",
-        framework="pt",           # Use PyTorch
-        device="cpu",
-        torch_dtype="auto"                
+        model=model,
+        tokenizer=tokenizer,
+        framework="pt",
+        device="cpu"
     )
